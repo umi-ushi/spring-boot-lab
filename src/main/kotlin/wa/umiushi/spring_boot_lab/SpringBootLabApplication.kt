6ea3e004692aa92d7deb5ae2bf343fda41d96846
umi-ushi <umiushi.me+dev@gmail.com>
2025-01-25 16:53:43 +0900

    refact: format by ktlint

--- src/main/kotlin/wa/umiushi/spring_boot_lab/SpringBootLabApplication.kt
+++ src/main/kotlin/wa/umiushi/spring_boot_lab/SpringBootLabApplication.kt
@@ -7,5 +7,5 @@ import org.springframework.boot.runApplication
 class SpringBootLabApplication
 
 fun main(args: Array<String>) {
-	runApplication<SpringBootLabApplication>(*args)
+    runApplication<SpringBootLabApplication>(*args)
 }
