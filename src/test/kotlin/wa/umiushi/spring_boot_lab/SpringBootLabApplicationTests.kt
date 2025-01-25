3b9e5bfb57c08b4fd706bf95cc7ea88715504967
umi-ushi <umiushi.me+dev@gmail.com>
2025-01-25 16:46:17 +0900

    add: spring-boot minimal dependency

---
+++ src/test/kotlin/wa/umiushi/spring_boot_lab/SpringBootLabApplicationTests.kt
@@ -0,0 +1,13 @@
+package wa.umiushi.spring_boot_lab
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootLabApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
